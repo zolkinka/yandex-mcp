0a26d3437f83f65cd59a83f73e98060b2e8db0bb
Kirill Zolkin <k.zolkin@sxope.com>
2026-01-15 19:18:39 +0300

    feat: implement log directory creation for specified LOG_DEST in logger configuration

--- dist/settings/logger.js
+++ dist/settings/logger.js
@@ -6,6 +6,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.logger = void 0;
 const pino_1 = __importDefault(require("pino"));
 const config_1 = require("./config");
+const fs_1 = require("fs");
+const path_1 = require("path");
+// Создаем директорию для логов если указан LOG_DEST
+if (config_1.config.LOG_DEST) {
+    const logDir = (0, path_1.dirname)(config_1.config.LOG_DEST);
+    if (!(0, fs_1.existsSync)(logDir)) {
+        (0, fs_1.mkdirSync)(logDir, { recursive: true });
+    }
+}
 exports.logger = (0, pino_1.default)({
     level: config_1.config.LOG_LEVEL,
     formatters: {
